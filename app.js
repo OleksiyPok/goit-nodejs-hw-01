3d3df5423346d8cef9bea5c75e6154f87dca915d
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-11-28 22:59:19 +0100

    comments have been deleted

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// const { program } = require("commander");
 const fs = require("fs").promises;
 
 const contacts = require("./contacts/contacts");
@@ -10,36 +9,22 @@ const invokeActon = async ({ action, id, data }) => {
   switch (action) {
     case "list":
       const contactsList = await contacts.getAllContacts();
-      // console.log("contactsList:", contactsList);
       return contactsList;
 
     case "get":
       const contact = await contacts.getContactById(id);
-      // if (!contact) {
-      //   throw new Error(`Contact with id=${id} not found`);
-      // }
-      // console.log("contact:", contact);
       return contact;
 
     case "add":
       const addedContact = await contacts.addContact(data);
-      // console.log("addedContact:", addedContact);
       return addedContact;
 
     case "update":
       const updatedContact = await contacts.updateContactById(id, data);
-      // if (!updatedContact) {
-      //   throw new Error(`Contact with id=${id} not found`);
-      // }
-      // console.log("updatedContact:", updatedContact);
       return updatedContact;
 
     case "remove":
       const removedContact = await contacts.removeContactById(id);
-      // if (!removedContact) {
-      //   throw new Error(`Contact with id=${id} not found`);
-      // }
-      // console.log("removedContact:", removedContact);
       return removedContact;
 
     default:
@@ -60,33 +45,3 @@ const options = program.opts();
 invokeActon(options)
   .then((data) => console.log(data))
   .catch((error) => console.log(error));
-
-// invokeActon({ action: "list" });
-
-// const contactId = "819fe1ba-9492-4a50-a8ec-78af18ba1195";
-// invokeActon({ action: "get", id: contactId });
-
-// const newContact = {
-//   name: "Fredy Krugger",
-//   email: "freddy.krugger@anyhome.ru",
-//   phone: "(795) 666-6666",
-// };
-// invokeActon({ action: "add", data: newContact });
-
-// const contactId = "819fe1ba-9492-4a50-a8ec-78af18ba1195";
-// invokeActon({
-//   action: "remove",
-//   id: contactId,
-// });
-
-// const contactId = "819fe1ba-9492-4a50-a8ec-78af18ba1195";
-// const updateContact = {
-//   name: "Fredy Krugger",
-//   email: "fredy.krugger@anyhome.ru",
-//   phone: "(795) 666-6666",
-// };
-// invokeActon({
-//   action: "update",
-//   id: contactId,
-//   data: updateContact,
-// });
