38a4685231398ad1e4fe9d06a9b86abaf1325344
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-11-28 22:52:13 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,92 @@
+// const { program } = require("commander");
+const fs = require("fs").promises;
+
+const contacts = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+
+const invokeActon = async ({ action, id, data }) => {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.getAllContacts();
+      // console.log("contactsList:", contactsList);
+      return contactsList;
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      // if (!contact) {
+      //   throw new Error(`Contact with id=${id} not found`);
+      // }
+      // console.log("contact:", contact);
+      return contact;
+
+    case "add":
+      const addedContact = await contacts.addContact(data);
+      // console.log("addedContact:", addedContact);
+      return addedContact;
+
+    case "update":
+      const updatedContact = await contacts.updateContactById(id, data);
+      // if (!updatedContact) {
+      //   throw new Error(`Contact with id=${id} not found`);
+      // }
+      // console.log("updatedContact:", updatedContact);
+      return updatedContact;
+
+    case "remove":
+      const removedContact = await contacts.removeContactById(id);
+      // if (!removedContact) {
+      //   throw new Error(`Contact with id=${id} not found`);
+      // }
+      // console.log("removedContact:", removedContact);
+      return removedContact;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <action>", "Choose action")
+  .option("-i, --id <id>", "user id")
+  .option("-n, --name <name>", "user name")
+  .option("-e, --email <email>", "user email")
+  .option("-p, --phone <phone>", "user phone");
+
+program.parse(process.argv);
+const options = program.opts();
+
+invokeActon(options)
+  .then((data) => console.log(data))
+  .catch((error) => console(error));
+
+// invokeActon({ action: "list" });
+
+// const contactId = "819fe1ba-9492-4a50-a8ec-78af18ba1195";
+// invokeActon({ action: "get", id: contactId });
+
+// const newContact = {
+//   name: "Fredy Krugger",
+//   email: "freddy.krugger@anyhome.ru",
+//   phone: "(795) 666-6666",
+// };
+// invokeActon({ action: "add", data: newContact });
+
+// const contactId = "819fe1ba-9492-4a50-a8ec-78af18ba1195";
+// invokeActon({
+//   action: "remove",
+//   id: contactId,
+// });
+
+// const contactId = "819fe1ba-9492-4a50-a8ec-78af18ba1195";
+// const updateContact = {
+//   name: "Fredy Krugger",
+//   email: "fredy.krugger@anyhome.ru",
+//   phone: "(795) 666-6666",
+// };
+// invokeActon({
+//   action: "update",
+//   id: contactId,
+//   data: updateContact,
+// });
---
+++ contacts.js
@@ -0,0 +1,68 @@
+const fs = require("fs").promises;
+const crypto = require("node:crypto");
+
+const { CONTACTS_FILE_PATH } = require("./filePaths");
+
+async function read() {
+  const data = await fs.readFile(CONTACTS_FILE_PATH, "utf-8");
+  return JSON.parse(data);
+}
+
+function write(data) {
+  return fs.writeFile(CONTACTS_FILE_PATH, JSON.stringify(data, null, 2));
+}
+
+const getAllContacts = async () => {
+  const contacts = read();
+  return contacts;
+};
+
+const getContactById = async (contactId) => {
+  const contactsList = await getAllContacts();
+  const contact = contactsList.find((item) => item.id === contactId);
+  if (!contact) {
+    return null;
+  }
+  return contact;
+};
+
+const addContact = async (data) => {
+  const contactsList = await getAllContacts();
+  const newId = crypto.randomUUID();
+  const newContact = { ...data, id: newId };
+  contactsList.push(newContact);
+  write(contactsList);
+  return newContact;
+};
+
+const updateContactById = async (contactId, data) => {
+  const contactsList = await getAllContacts();
+  const idx = contactsList.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  contactsList[idx] = { ...data, id: contactId };
+  write(contactsList);
+  return contactsList[idx];
+};
+
+const removeContactById = async (contactId) => {
+  const contactsList = await getAllContacts();
+  const idx = contactsList.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const newContactsList = contactsList.filter((_, index) => index !== idx);
+  write(newContactsList);
+  return contactsList[idx];
+};
+
+const contacts = {
+  getAllContacts,
+  getContactById,
+  addContact,
+  updateContactById,
+  removeContactById,
+};
+
+module.exports = contacts;
